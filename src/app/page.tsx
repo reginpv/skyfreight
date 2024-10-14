f76c4e5ec4bd9f700eecff2facc47404e5d048b6
reginpv <reginpv@gmail.com>
2024-10-15 04:42:43 +0800

    initial commit with next

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">Wep app coming soon.</div>
+  );
+}
