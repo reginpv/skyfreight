cded0d015500d2cd88f22d39e9d35180b58b6811
reginpv <reginpv@gmail.com>
2024-11-02 07:09:30 +0800

    fix styling nesting

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return(
     <footer>
-      <div>
+      <div className="footer__container container container--custom">
         <div>
           Footer
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header>
-      <div className="header__container">
+      <div className="header__container container container--custom">
         <div className="header__content flex justify-between gap-5">
           <div>Logo</div>
           <nav>Nav</nav>
