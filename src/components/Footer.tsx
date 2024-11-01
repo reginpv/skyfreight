aee4bff10b80e760d80d4752c196a5f31389e4b4
reginpv <reginpv@gmail.com>
2024-11-02 07:26:24 +0800

    foote

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return(
     <footer>
       <div className="footer__container container container--custom">
-        <div>
-          Footer
+        <div className="text-center">
+          &copy; {new Date().getFullYear()} - Sky Freight Oman
         </div>
       </div>
     </footer>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image"
 export default function Header() {
   return (
     <header>
       <div className="header__container container container--custom">
         <div className="header__content flex justify-between gap-5">
-          <div>Logo</div>
+          <div>
+            LOGO
+          </div>
           <nav>Nav</nav>
         </div>
       </div>
