982b2c561f8d23963cc3424afab4e05a24c66727
reginpv <reginpv@gmail.com>
2024-11-02 17:13:40 +0800

    default pages

---
+++ src/app/account/page.tsx
@@ -0,0 +1,15 @@
+import TemplateDefault from "@/components/templates/Default"
+
+export default function Home() {
+  return (
+    <TemplateDefault>
+      <section>
+        <div className="container container--custom">
+          <div>
+            Coming soon...
+          </div>
+        </div>
+      </section>
+    </TemplateDefault>
+  );
+}
