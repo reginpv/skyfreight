6d3ababa3a8c88d0c2b699619d6e557134884f79
reginpv <reginpv@gmail.com>
2024-11-16 00:00:44 +0800

    update

---
+++ src/app/login/page.tsx
@@ -0,0 +1,15 @@
+import TemplateBlank from "@/components/templates/Blank"
+
+export default function Login() {
+  return (
+    <TemplateBlank>
+      <section>
+        <div className="container container--custom">
+          <div>
+            Coming soon...
+          </div>
+        </div>
+      </section>
+    </TemplateBlank>
+  );
+}
---
+++ src/components/templates/Blank.tsx
@@ -0,0 +1,8 @@
+export default function TemplateBlank({ children }: Props) {
+  
+  return (
+    <>
+      <main className="flex-1">{children}</main>
+    </>
+  )
+}
