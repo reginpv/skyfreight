08034cc0bb12ceb0cd44ed1374218c3bbefa3cd9
reginpv <reginpv@gmail.com>
2024-12-07 18:37:04 +0800

    blog section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import TemplateDefault from "@/components/templates/Default"
 import StatCount from "@/components/StatCount"
+import BlogSlider from "@/components/BlogSlider"
 
 export default async function Home() {
 
   return (
     <TemplateDefault>
+      <BlogSlider />
       <StatCount />
     </TemplateDefault>
   )
---
+++ src/components/BlogSlider.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import data from "@/data/blog.json"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+
+export default function BlogSlider() {
+  return (
+    <section className="blog-slider ~py-[30px]/[60px] bg-gray-100">
+      <div className="blog-container container container--custom">
+        <div className="blog-content flex flex-col ~gap-[12px]/[24px]">
+
+          <div>
+            <h2>News and Updates</h2>
+          </div>
+          
+          <div>
+            <div className="blog-swiper-container">
+              <Swiper
+                spaceBetween={30} 
+                slidesPerView={4}
+                loop={true}
+                pagination={{ clickable: true }} // Enable pagination
+              >
+                {
+                  data.map((b, i)=>(
+                    <SwiperSlide key={i} className="!h-auto">
+                      <div className="flex h-full flex-col gap-3 ~p-[10px]/[20px] border rounded-2xl overflow-hidden bg-white">
+                        <div className="slide-content font-semibold">
+                          {b.title}
+                        </div>
+                        <div>
+                          {b.content}
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                  ))
+                }
+              </Swiper>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
