ddf747d505841cc937db21ee651b47baddbca069
reginpv <reginpv@gmail.com>
2024-12-08 11:18:54 +0800

    updates

---
+++ src/components/BannerHome.tsx
@@ -0,0 +1,20 @@
+export default function BannerHome() {
+  return (
+    <section className="banner-home">
+      <div className="banner-home__container container container--custom">
+        <div className="banner-home__content">
+          
+          <div>
+            <div>
+
+            </div>
+            <div>
+              
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
