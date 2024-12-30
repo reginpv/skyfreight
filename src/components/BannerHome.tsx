e0df518d43c535dfa320c19023d2609cd63f68ab
reginpv <reginpv@gmail.com>
2024-12-31 04:48:25 +0800

    banner

--- src/components/BannerHome.tsx
+++ src/components/BannerHome.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link"
+import { Icon } from "@/components/ui/Icons"
+import Image from "next/image"
 
 export default function BannerHome() {
 
@@ -7,18 +9,20 @@ export default function BannerHome() {
   return (
     <section className="banner-home">
       <div className="banner-home__container">
-        <div className="banner-home__content ">
+        <div className="banner-home__content bg-gray-200">
           
           <div>
 
             {/** Top */}
-            <div className="bg-gray-200 ~pb-[40px]/[100px] ~pt-[134px]/[234px] -mt-[134px]">
+            <div className="banner-home__top bg-gray-200 ~pt-[164px]/[200px] -mt-[134px] pb-[150px] lg:pb-[200px] -mb-[120px] relative">
 
               <div className="container container--custom flex flex-col md:flex-row gap-10">
 
                 {/** Top left */}
-                <div className="flex-1">
-
+                <div className="flex-1 hidden lg:block">
+                  <div className="bg-primary absolute aspect-[1/1] w-[500px] rounded-tr-[100px] -rotate-[9deg] -ml-[30px] -mt-[20px]">
+                  </div>
+                  <Image src="/images/services-box.jpg" width={510} height={300} alt="box" className="absolute bottom-[50px] -rotate-[4deg] border-l-2 rounded-tr-[100px] border-t-[5px] border-r-[5px] border-secondary -ml-[60px]" />
                 </div>
 
                 {/** Top right */}
@@ -47,16 +51,23 @@ export default function BannerHome() {
             </div>
 
             {/** Bottom */}
-            <div className="banner-home__bottom py-[60px] bg-primary text-white">
+            <div className="banner-home__bottom ~pt-[120px]/[140px] ~pb-[80px]/[100px] bg-primary text-white relative">
+
               <div className="container container--custom flex flex-col gap-3 ~text-[18px]/[22px] uppercase font-semibold">
                 <div>
                   <div>We offer:</div>
                 </div>
                 <div>
-                  <Link href="/product/air-freight" className="button button--outlined button--md">Air Freight</Link>
+                  <Link href="/product/air-freight" className="button button--outlined h-[64px] inline-flex gap-2 py-1 pl-1 pr-5">
+                    <span className="inline-flex items-center justify-center"><Icon icon="airplane" width={48} height={48} className="border-[3px] border-white rounded-full p-2 fill-white" /></span>
+                    <span>Air Freight</span>
+                  </Link>
                 </div>
                 <div>
-                  <Link href="/product/sea-freight" className="button button--outlined button--md">Sea Freight</Link>
+                  <Link href="/product/sea-freight" className="button button--outlined h-[64px] inline-flex gap-2 py-1 pl-1 pr-5">
+                    <span className="inline-flex items-center justify-center"><Icon icon="boat" width={48} height={48} className="border-[3px] border-white rounded-full p-2 fill-white" /></span>
+                    <span>Sea Freight</span>
+                  </Link>
                 </div>
                 <div className="mt-5">
                   <Link href="/products">Learn More</Link>
